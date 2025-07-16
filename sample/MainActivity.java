8cb03176cdfdd60bcd6a8d06ba2c85b8919d38eb
ansarmca <ansarkp300@gmail.com>
2025-07-16 15:39:53 +0530

    Update MainActivity.java

--- sample/MainActivity.java
+++ sample/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.alc2025;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("lifecycle","onCreate invoked");
+    }
+    protected void onStart(){
+        super.onStart();
+        Log.d("lifecycle","onStart invoked");
+    }
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifecycle","onResume invoked");
+    }
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifecycle","onPause invoked");
+    }
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifecycle","onStop invoked");
+    }
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifecycle","onDestroy invoked");
     }
 }
